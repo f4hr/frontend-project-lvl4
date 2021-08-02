74499bfded1f43decd8971c659792439a1cddd86
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-02 13:24:08 +0500

    refactor: add loading state handling

--- src/features/chat/Chat.jsx
+++ src/features/chat/Chat.jsx
@@ -8,55 +8,87 @@ import {
   Nav,
 } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchChat } from './chatSlice.js';
+import { setInitialState } from './channelsSlice.js';
 import NewMessageForm from './NewMessageForm.jsx';
 
-const Channels = ({ channels, currentChannelId }) => (
-  <Nav className="flex-column" variant="pills" activeKey={currentChannelId}>
-    {channels.map(({ id, name }) => (
-      <Nav.Item key={id}>
-        <Nav.Link eventKey={id} href={`#${id}`}>
-          {name}
-        </Nav.Link>
-      </Nav.Item>
-    ))}
-  </Nav>
-);
+const Channels = () => {
+  const {
+    byId,
+    allIds,
+    currentChannelId,
+    status,
+  } = useSelector((state) => state.channels);
 
-const Messages = ({ messages }) => (
-  <ul>
-    {messages.map(({ id, user, message }) => <li key={id}>{`<b>${user}</b>: ${message}`}</li>)}
-  </ul>
-);
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return <div>Failed to load channels</div>;
+  }
+
+  return (
+    <Nav className="flex-column" variant="pills" activeKey={currentChannelId}>
+      {allIds.map((id) => (
+        <Nav.Item key={id}>
+          <Nav.Link eventKey={id} href={`#${id}`}>
+            {byId[id].name}
+          </Nav.Link>
+        </Nav.Item>
+      ))}
+    </Nav>
+  );
+};
+
+const Messages = () => {
+  const { currentChannelId } = useSelector((state) => state.channels);
+  const { byId, allIds, status } = useSelector((state) => state.messages);
+
+  if (status === 'pending') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return <div>Failed to load messages</div>;
+  }
+
+  if (allIds.length === 0) {
+    return <div>No messages in this channel</div>;
+  }
+
+  const filteredMessages = allIds
+    .map((id) => byId[id])
+    .filter(({ channelId }) => channelId === currentChannelId);
+
+  return (
+    <ul className="list-unstyled">
+      {filteredMessages.map(({ id, username, body }) => (
+        <li key={id}>
+          <b>{`${username}: `}</b>
+          {body}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Chat = () => {
-  const { channels, messages, currentChannelId } = useSelector((state) => state.chat);
-  const chatStatus = useSelector((state) => state.chat.status);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (chatStatus === 'idle') {
-      dispatch(fetchChat());
-    }
-  }, [chatStatus, dispatch]);
-
-  const filteredMessages = messages.filter(({ channelId }) => channelId === currentChannelId);
+    dispatch(setInitialState());
+  }, []);
 
   return (
     <Container fluid className="h-100 px-0">
       <Row className="h-100 m-0">
         <Col xs lg="2" className="p-3 bg-light">
           <h6>Channels</h6>
-          <Channels
-            channels={channels}
-            currentChannelId={currentChannelId}
-          />
+          <Channels />
         </Col>
         <Col className="d-flex flex-column h-100 p-3">
-          <div className="flex-grow-1">
-            {(messages.length === 0)
-              ? <p>Chat is empty...</p>
-              : <Messages messages={filteredMessages} />}
+          <div className="chat__messages-wrapper flex-grow-1">
+            <Messages />
           </div>
           <NewMessageForm />
         </Col>
