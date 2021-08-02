47eedbf4cc39b535c2ffa3e3abade160471589be
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-02 12:42:09 +0500

    refactor: rename chat initial state endpoint

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const host = '';
 const prefix = 'api/v1';
 
 export default {
-  chatPath: () => [host, prefix, 'data'].join('/'),
+  initialStatePath: () => [host, prefix, 'data'].join('/'),
   loginPath: () => [host, prefix, 'login'].join('/'),
   channelsPath: () => [host, prefix, 'channels'].join('/'),
   channelPath: (id) => [host, prefix, 'channels', id].join('/'),
