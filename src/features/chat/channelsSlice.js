469dc4d29d9be6a7648139b2abf11489bce70c9d
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-02 13:01:28 +0500

    feat: add send and get message handling
    split chatSlice into separate slices
    normalize state messages and channels structure

---
+++ src/features/chat/channelsSlice.js
@@ -0,0 +1,63 @@
+// @ts-check
+
+import { keyBy } from 'lodash';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import routes from '../../routes.js';
+
+const initialState = {
+  byId: {},
+  allIds: [],
+  currentChannelId: null,
+  status: 'idle',
+  error: null,
+};
+
+const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'));
+
+  if (userId && userId.token) {
+    return { Authorization: `Bearer ${userId.token}` };
+  }
+
+  return {};
+};
+
+export const setInitialState = createAsyncThunk('channels/setInitialState', async () => {
+  const response = await axios.get(routes.initialStatePath(), {
+    headers: getAuthHeader(),
+  });
+
+  return response.data;
+});
+
+export const channelsSlice = createSlice({
+  name: 'channels',
+  initialState,
+  reducers: {
+    addChannel: (state, action) => {
+      console.log(action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(setInitialState.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(setInitialState.fulfilled, (state, action) => {
+        const { channels, currentChannelId } = action.payload;
+        state.currentChannelId = currentChannelId;
+        state.allIds = channels.map(({ id }) => id);
+        state.byId = keyBy(channels, 'id');
+        state.status = 'succeeded';
+      })
+      .addCase(setInitialState.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { addChannel } = channelsSlice.actions;
+
+export default channelsSlice.reducer;
---
+++ src/features/chat/messagesSlice.js
@@ -0,0 +1,77 @@
+// @ts-check
+
+import { keyBy } from 'lodash';
+import { createSlice } from '@reduxjs/toolkit';
+import { setInitialState } from './channelsSlice.js';
+
+const initialState = {
+  byId: {},
+  allIds: [],
+  status: 'idle',
+  error: null,
+};
+
+const sendMessageActions = {
+  request: 'newMessage',
+  success: 'sendMessageSuccess',
+  failure: 'sendMessageFailure',
+};
+
+export const messagesSlice = createSlice({
+  name: 'messages',
+  initialState,
+  reducers: {
+    newMessage: (state, action) => {
+      const {
+        id,
+        channelId,
+        username,
+        body,
+      } = action.payload;
+
+      state.allIds.push(id);
+      state.byId[id] = {
+        id,
+        channelId,
+        username,
+        body,
+      };
+    },
+    sendMessage: {
+      reducer: (state) => {
+        state.status = 'pending';
+      },
+      prepare: (message) => ({
+        payload: {
+          type: 'socket',
+          scope: 'messages',
+          actions: sendMessageActions,
+          body: message,
+        },
+      }),
+    },
+    [sendMessageActions.success]: (state) => {
+      state.status = 'succeeded';
+    },
+    [sendMessageActions.failure]: (state) => {
+      state.status = 'failed';
+      state.error = 'Message send failed';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(setInitialState.fulfilled, (state, action) => {
+        const { messages } = action.payload;
+        state.allIds = messages.map(({ id }) => id);
+        state.byId = keyBy(messages, 'id');
+      });
+  },
+});
+
+export const serverActions = [
+  'newMessage',
+];
+
+export const { actions } = messagesSlice;
+
+export default messagesSlice.reducer;
