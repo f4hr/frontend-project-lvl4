1e40c689dd128446cab5d970af1af3db959f048b
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-02 13:22:11 +0500

    feat: add message send
    refactor form submitting and validation state handling

--- src/features/chat/NewMessageForm.jsx
+++ src/features/chat/NewMessageForm.jsx
@@ -1,43 +1,76 @@
 // @ts-check
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Button } from 'react-bootstrap';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
+import useAuth from '../../common/hooks/index.jsx';
+import { actions } from './messagesSlice.js';
 
 const NewMessageForm = () => {
-  const [formState] = useState('filling');
+  const dispatch = useDispatch();
+  const { currentChannelId } = useSelector((state) => state.channels);
+  const { status } = useSelector((state) => state.messages);
+  const inputRef = useRef();
+  const { username } = useAuth();
+  const initialValues = { message: '' };
 
-  const handleMessageSend = (values) => {
-    console.log(values);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleMessageSend = (values, { resetForm, setSubmitting }) => {
+    const body = values.message.trim();
+    const message = {
+      channelId: currentChannelId,
+      username,
+      body,
+    };
+    dispatch(actions.sendMessage(message));
+
+    setSubmitting(false);
+    resetForm({ values: initialValues });
   };
+
+  const schema = Yup.object({
+    message: Yup.string()
+      .trim('Message containing only whitespaces is not allowed')
+      .required('Required field'),
+  });
 
   return (
     <Formik
+      validationSchema={schema}
       onSubmit={handleMessageSend}
-      initialValues={{
-        message: '',
-      }}
+      initialValues={initialValues}
     >
       {({
         handleSubmit,
         handleChange,
         values,
+        errors,
       }) => (
         <Form className="d-flex" noValidate onSubmit={handleSubmit}>
-          <Form.Group className="flex-grow-1 mb-0 mr-3" controlId="username">
+          <Form.Group className="flex-grow-1 mb-0 mr-3" controlId="message">
             <Form.Control
               type="text"
               name="message"
               value={values.message}
               placeholder="Enter message..."
+              ref={inputRef}
               onChange={handleChange}
-              readOnly={formState === 'processing'}
+              readOnly={status === 'pending'}
             />
           </Form.Group>
           <Button
             type="submit"
             variant="primary"
-            disabled={values.message === '' || formState === 'processing'}
+            disabled={
+              !!errors.message
+              || values.message === ''
+              || status === 'pending'
+            }
           >
             Send
           </Button>
