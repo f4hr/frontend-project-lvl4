1269e1a9709d8fb2cf54c984bb713a263a3dfc2f
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-03 14:51:49 +0500

    feat(modals): add modals reducer

--- src/app/store.js
+++ src/app/store.js
@@ -4,11 +4,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import socketMiddleware from '../common/middlewares/socketMiddleware.js';
 import channelsReducer from '../features/chat/channelsSlice.js';
 import messagesReducer from '../features/chat/messagesSlice.js';
+import modalsReducer from '../features/modals/modalsSlice.js';
 
 const store = configureStore({
   reducer: {
     channels: channelsReducer,
     messages: messagesReducer,
+    modals: modalsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(socketMiddleware()),
