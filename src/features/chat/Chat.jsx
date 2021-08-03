54b3fd9abf87f6390eec4168489562b993a9fd4e
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-03 20:25:44 +0500

    feat: add channel remove and rename buttons
    add custom scrollbars to channels and messages container

--- src/features/chat/Chat.jsx
+++ src/features/chat/Chat.jsx
@@ -1,18 +1,26 @@
 // @ts-check
 
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   Container,
   Row,
   Col,
   Nav,
   Button,
+  ButtonGroup,
+  Dropdown,
 } from 'react-bootstrap';
 import { GoPlus } from 'react-icons/go';
-import { useSelector, useDispatch } from 'react-redux';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { setInitialState, setCurrentChannel } from './channelsSlice.js';
 import NewMessageForm from './NewMessageForm.jsx';
-import { openModal, newChannelModal } from '../modals/modalsSlice.js';
+import {
+  openModal,
+  newChannelModal,
+  removeChannelModal,
+  renameChannelModal,
+} from '../modals/modalsSlice.js';
 
 const Channels = () => {
   const dispatch = useDispatch();
@@ -31,18 +39,57 @@ const Channels = () => {
     return <div>Failed to load channels</div>;
   }
 
-  const handleSelect = (channelId, e) => {
-    e.preventDefault();
+  const handleChannelChange = (channelId) => () => {
     dispatch(setCurrentChannel(channelId));
   };
 
+  const handleRemoveChannel = (id) => (e) => {
+    e.preventDefault();
+    const data = {
+      type: removeChannelModal(),
+      meta: {
+        channelId: id,
+      },
+    };
+    dispatch(openModal(data));
+  };
+
+  const handleRenameChannel = (id) => (e) => {
+    e.preventDefault();
+    const data = {
+      type: renameChannelModal(),
+      meta: {
+        channelId: id,
+      },
+    };
+    dispatch(openModal(data));
+  };
+
   return (
-    <Nav className="flex-column" variant="pills" activeKey={currentChannelId}>
+    <Nav className="flex-column pr-1" variant="pills" activeKey={currentChannelId}>
       {allIds.map((id) => (
-        <Nav.Item key={id}>
-          <Nav.Link eventKey={id} href={`#${id}`} onSelect={handleSelect}>
-            {byId[id].name}
-          </Nav.Link>
+        <Nav.Item key={id} className="w-100">
+          <Dropdown className="w-100" as={ButtonGroup}>
+            <Button
+              className="w-100 text-left text-truncate"
+              variant={(currentChannelId === id) ? 'primary' : 'link'}
+              type="button"
+              onClick={handleChannelChange(id)}
+            >
+              {byId[id].name}
+            </Button>
+            {(byId[id].removable)
+              ? (
+                <>
+                  <Dropdown.Toggle split variant={(currentChannelId === id) ? 'primary' : 'link'} />
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#" onClick={handleRemoveChannel(id)}>Remove</Dropdown.Item>
+                    <Dropdown.Item href="#" onClick={handleRenameChannel(id)}>Rename</Dropdown.Item>
+                  </Dropdown.Menu>
+                </>
+              )
+              : null}
+          </Dropdown>
         </Nav.Item>
       ))}
     </Nav>
@@ -70,7 +117,7 @@ const Messages = () => {
   }
 
   return (
-    <ul className="list-unstyled">
+    <ul className="list-unstyled text-break">
       {filteredMessages.map(({ id, username, body }) => (
         <li key={id}>
           <b>{`${username}: `}</b>
@@ -89,7 +136,7 @@ const Chat = () => {
   }, []);
 
   const handleNewChannel = () => {
-    dispatch(openModal(newChannelModal()));
+    dispatch(openModal({ type: newChannelModal() }));
   };
 
   return (
@@ -109,12 +156,14 @@ const Chat = () => {
               <span className="sr-only">Add new channel</span>
             </Button>
           </div>
-          <Channels />
+          <Scrollbars style={{ height: 'calc(100% - 40px)' }}>
+            <Channels />
+          </Scrollbars>
         </Col>
         <Col className="d-flex flex-column h-100 p-3">
-          <div className="chat__messages-wrapper flex-grow-1">
+          <Scrollbars>
             <Messages />
-          </div>
+          </Scrollbars>
           <NewMessageForm />
         </Col>
       </Row>
