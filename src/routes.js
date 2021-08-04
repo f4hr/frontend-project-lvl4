9c19d6ebcec93e51add022bfd5b57c00853fb483
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-04 17:06:22 +0500

    feat: add signup endpoint

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const prefix = 'api/v1';
 export default {
   initialStatePath: () => [host, prefix, 'data'].join('/'),
   loginPath: () => [host, prefix, 'login'].join('/'),
+  signupPath: () => [host, prefix, 'signup'].join('/'),
   channelsPath: () => [host, prefix, 'channels'].join('/'),
   channelPath: (id) => [host, prefix, 'channels', id].join('/'),
   channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
