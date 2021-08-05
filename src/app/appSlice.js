e0f4aa41361420b7cc36fb5fa8d43390a33d5b25
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-05 11:28:01 +0500

    feat: add app state slice

---
+++ src/app/appSlice.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  lang: 'ru',
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    changeLanguage: (state, action) => {
+      state.lang = action.payload.lang;
+    },
+  },
+});
+
+export const { changeLanguage } = appSlice.actions;
+export default appSlice.reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import socketMiddleware from '../common/middlewares/socketMiddleware.js';
+import appReducer from './appSlice.js';
 import channelsReducer from '../features/chat/channelsSlice.js';
 import messagesReducer from '../features/chat/messagesSlice.js';
 import modalsReducer from '../features/modals/modalsSlice.js';
 
 const store = configureStore({
   reducer: {
+    app: appReducer,
     channels: channelsReducer,
     messages: messagesReducer,
     modals: modalsReducer,
