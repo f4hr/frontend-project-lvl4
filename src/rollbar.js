b713c5f943fdcb2101af171df1962a9ccf420281
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-05 17:50:13 +0500

    ci: add rollbar environment config option

--- src/rollbar.js
+++ src/rollbar.js
@@ -4,4 +4,7 @@ export default () => new Rollbar({
   accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
 });
