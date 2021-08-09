15f89c2057889aec6dc21bbc7d4f22e6561a143d
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-08-09 16:32:26 +0500

    text: enable app initialization

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
-// import io from 'socket.io-client';
-// import { render } from 'react-dom';
+import io from 'socket.io-client';
+import { render } from 'react-dom';
 import './styles/application.scss';
 import init from './init.jsx';
 
-// init(io()).then((vdom) => render(vdom, document.querySelector('#chat')));
+init(io()).then((vdom) => render(vdom, document.querySelector('#chat')));
 
 export default init;
