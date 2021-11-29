0c60682ee565c44ef298e54196648dd787e85704
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-11-29 22:53:23 +0500

    test: update tests

---
+++ __mocks__/localStorageMock.js
@@ -0,0 +1,22 @@
+const localStorageMock = (() => {
+  let store = {}; // eslint-disable-line
+
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
